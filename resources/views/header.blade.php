8de9c08040f1bcaa522d56512c604a2e2b292445
bh10-d <duchieubui25012002@gmail.com>
2021-11-10 23:28:47 +0700

    edit head foot css

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,21 +2,22 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="{{asset('css/style1.css')}}">
 </head>
 
 <body>
-    <div class="header__wrap"><!--edit-->
-        <div class="header__wrap--p1"><!--edit-->
+    <div class="header__wrap">
+        <!--edit-->
+        <div class="header__wrap--p1">
+            <!--edit-->
             <div class="header__wrap--p1e">
                 <a href="{{ url('/') }}" class="header__logo"><img class="logo" src="{{asset('image/logo.png')}}" alt="logo"></a>
-                <div class="header__search"><!--edit-->
-                    <div class="header__search--list">
+                <div class="header__search">
+                    <!--edit-->
+                    <div class="header__search--listt">
                         <div class="header__search-droplist">
-                            <a onclick="select();" class="select">Chọn danh mục <i
-                                    class="fas fa-chevron-circle-down"></i></a>
+                            <a onclick="select();" class="select-hieu">Chọn danh mục <i class="fas fa-chevron-circle-down"></i></a>
                             <div class="drop">
                                 <a href="#">Điện thoại - Máy tính bảng</a>
                                 <a href="#">Laptop - Laptop Gaming</a>
@@ -29,7 +30,8 @@
                         <button type="submit" class="btn"><i class="fas fa-search"></i></button>
                     </div>
                 </div>
-                <p class="header__hotline"><!--edit-->
+                <p class="header__hotline">
+                    <!--edit-->
                     <i class="fas fa-phone-alt"></i>
                     Hotline:
                     <span>113 113</span>
@@ -40,30 +42,41 @@
                         @if(Auth::user()==null)
                         {{ __('Tài khoản') }}
                         @else
-                        <p>Xin chào: {{Auth::user()->username}}</p>
-                        @endif
+                    <p>Xin chào: {{Auth::user()->username}}</p>
+                    @endif
                     </p>
                     <div class="header__account--option">
                         @guest
-                            <p><a style="text-decoration:none;font-weight: 500;" href="{{ route('login') }}">Đăng nhập</a></p>
-                            <p><a style="text-decoration:none;font-weight: 500;" href="{{ route('register') }}">Đăng kí</a></p>
+                        <p><a style="text-decoration:none;font-weight: 500;" href="{{ route('login') }}">Đăng nhập</a></p>
+                        <p><a style="text-decoration:none;font-weight: 500;" href="{{ route('register') }}">Đăng kí</a></p>
                         @else
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
-                                {{ __('Đăng xuất') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+                            {{ __('Đăng xuất') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                         @endguest
                     </div>
                 </div>
                 <div class="header__cart">
-                    <p><i class="fas fa-shopping-cart"></i> Giỏ hàng</p>
-                    <div class="cart--count">
-                        <span>1</span> <!--so luong san pham------------------------------------------------------------------------------------------------>
+                    <div onclick="cartbutton();">
+                        <p><i class="fas fa-shopping-cart"></i> Giỏ hàng</p>
+                        @if (Session::get('cart'))
+                        <div class="cart--count">
+                            @php $product=0 @endphp
+                            @php $list=0 @endphp
+                            @foreach (Session::get('cart') as $id => $details)
+                            @php $product = $details['product_qty'] @endphp
+                            @php $list+=$product @endphp
+                            @endforeach
+                            <span>{{ $list }}</span>
+                            <!--so luong san pham------------------------------------------------------------------------------------------------>
+                        </div>
+                        @endif
                     </div>
+
                     <div class="header__cart--list">
                         <form action="">
                             <div class="header__cart--item">
@@ -73,7 +86,7 @@
                                     <span class="item--price">35.990.000đ</span>
                                     <div class="item--quantity">
                                         <a class="btn item minus" onclick="quanityminus();">-</a>
-                                        <input type="text" class="quantity" value="1">
+                                        <input type="text" class="quantityy" value="1">
                                         <a class="btn item plus" onclick="quantityplus();">+</a>
                                     </div>
                                     <!-- <span class="item--quantity" type="input"></span> -->
@@ -90,13 +103,24 @@
                 </div>
             </div>
         </div>
-        <div class="header__wrap--p2"><!--edit-->
+        <div class="header__wrap--p2">
+            <!--edit-->
             <label for="nav_check_input" class="head__navbarrespon--bars"><i class="fas fa-bars"></i></label>
-            <div class="head__navbarrespon--cart">
-                <p ><i class="fas fa-shopping-cart"></i></p>
+            <div class="head__navbarrespon--cart" onclick="cartbutton();">
+                <p><i class="fas fa-shopping-cart"></i></p>
+                @if (Session::get('cart'))
                 <div class="cart--count">
-                    <span>1</span>
+                    <!-- <span>1</span> -->
+                    @php $product=0 @endphp
+                    @php $list=0 @endphp
+                    @foreach (Session::get('cart') as $id => $details)
+                    @php $product = $details['product_qty'] @endphp
+                    @php $list+=$product @endphp
+                    @endforeach
+                    <span>{{ $list }}</span>
+                    <!--so luong san pham------------------------------------------------------------------------------------------------>
                 </div>
+                @endif
             </div>
             <input type="checkbox" hidden name="" class="nav__check" id="nav_check_input">
             <label for="nav_check_input" class="navbarrespon-overlay"></label>
@@ -106,13 +130,13 @@
                         <div class="user__info--table">
                             <span><img src="image/logo.png" style="width:40px;height:40px;" alt="logo"></span>
                             <div style="display:block; width:240px; height:20px">
-                            @guest
+                                @guest
                                 <p><a style="text-decoration:none;font-weight: 500; color: white;" href="{{ route('login') }}">Đăng nhập</a></p>
                                 <p><a style="text-decoration:none;font-weight: 500; color: white;" href="{{ route('register') }}">Đăng kí</a></p>
-                            @else
+                                @else
                                 <p>Xin chào: {{Auth::user()->username}}</p>
                                 <p>Email: {{Auth::user()->email}}</p>
-                            @endguest
+                                @endguest
                             </div>
                         </div>
                     </div>
@@ -124,19 +148,18 @@
                     <ul class="header__navbarrespon--list">
                         <li><a href="#">1</a></li>
                         <li><a style="cursor: pointer;" onclick="dropdownnav();">Danh mục sản phẩm </a><span class="arrow"><i class="fas fa-arrow-down"></i>
-                            <ul class="responsive__nav--list">
-                                <li><a href="#">Điện thoại - Máy tính bảng</a> </li>
-                                <li><a href="#">Laptop - Laptop Gaming</a> </li>
-                                <li><a href="#">Thiết bị văn phòng</a> </li>
-                                <li><a href="#">Kỹ thuật số</a> </li>
-                                <li><a href="#">Phụ kiện</a> </li>
-                            </ul>
-                        </span></li>
+                                <ul class="responsive__nav--list">
+                                    <li><a href="#">Điện thoại - Máy tính bảng</a> </li>
+                                    <li><a href="#">Laptop - Laptop Gaming</a> </li>
+                                    <li><a href="#">Thiết bị văn phòng</a> </li>
+                                    <li><a href="#">Kỹ thuật số</a> </li>
+                                    <li><a href="#">Phụ kiện</a> </li>
+                                </ul>
+                            </span></li>
                         <!-- <li><a href="#">Tài khoản</a></li> -->
                         <li>
                             @if(Auth::user()!=null)
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
                                 {{ __('Đăng xuất') }}
                             </a>
